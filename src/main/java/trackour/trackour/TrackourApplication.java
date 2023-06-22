07ce8341eb2c6d781bb33260ee6ad787d1809e63
TGohar <gohartallal@hotmail.com>
2023-06-21 18:33:38 -0700

    initialized project

---
+++ src/main/java/trackour/trackour/TrackourApplication.java
@@ -0,0 +1,13 @@
+package trackour.trackour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackourApplication.class, args);
+	}
+
+}
---
+++ src/test/java/trackour/trackour/TrackourApplicationTests.java
@@ -0,0 +1,13 @@
+package trackour.trackour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
