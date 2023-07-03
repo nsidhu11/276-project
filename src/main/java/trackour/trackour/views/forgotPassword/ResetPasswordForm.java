45dbaf1f8f597388155da9cddf857e151d29ebc0
ellabgreen <ellabgreen2014@gmail.com>
2023-07-03 16:49:51 -0700

    feature: reset password

---
+++ src/main/java/trackour/trackour/views/forgotPassword/ResetPasswordForm.java
@@ -0,0 +1,131 @@
+package trackour.trackour.views.forgotPassword;
+
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.html.H3;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.PasswordField;
+import com.vaadin.flow.data.binder.Binder;
+import com.vaadin.flow.data.binder.ValidationException;
+import com.vaadin.flow.router.RouterLink;
+
+import trackour.trackour.models.CustomUserDetailsService;
+import trackour.trackour.models.User;
+import trackour.trackour.views.login.LoginPage;
+
+public class ResetPasswordForm extends VerticalLayout {
+    CustomUserDetailsService userService;
+
+    User user;
+
+    private H3 title;
+    
+    private PasswordField passwordField;
+    private PasswordField passwordConfirmField;
+
+    private Span errorMessageField;
+
+    private Button submit;
+
+    RouterLink loginLink;
+
+    public ResetPasswordForm(CustomUserDetailsService userService, User user){
+        this.userService = userService;
+        this.user = user;
+
+        this.title = new H3("Reset your password");
+
+        this.passwordField = new PasswordField("New Password");
+        this.passwordConfirmField = new PasswordField("Confirm Password");
+
+        this.passwordField.setRequiredIndicatorVisible(true);
+        this.passwordConfirmField.setRequiredIndicatorVisible(true);
+
+        this.errorMessageField = new Span();
+
+        this.submit = new Button("Reset Password");
+        this.submit.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+
+        //link to login page
+        this.loginLink = new RouterLink("return to login page", LoginPage.class);
+
+        add(title, doFormLayout(), errorMessageField, loginLink);
+
+        Binder<User> binder = new Binder<>(User.class);
+
+        binder.readBean(user);
+
+        System.out.println("Binding user " + user.getUsername());
+
+        doBindFormToValidationRules(binder);
+
+        submit.addClickListener(ev -> {
+                System.out.println("Submit pressed!!");
+
+                try {
+                        // perform validation
+                        // update the newUser object with data from the fields
+                        binder.writeBean(user);
+                } catch (ValidationException e) {
+                        System.out.println("Validation ERROR: Check that all validation rules are passed!");
+                        UI.getCurrent().navigate("signup?error");
+                        return;
+                }
+                if (binder.isValid()) {
+                        System.out.println("Signup validations all passed!");
+                        // if no errors occured during validation, register/call userservice registration method
+                        System.out.println("Registering user is:");
+                        user.setPasswordResetToken(null);
+                        userService.updatePassword(user);
+                        UI.getCurrent().navigate("redirect:/login");
+                }
+        });
+    }
+    
+    private FormLayout doFormLayout() {
+        FormLayout layout = new FormLayout();
+
+        setMaxWidth("500px");
+
+        setMargin(true);
+
+        setAlignItems(FlexComponent.Alignment.CENTER);
+
+        layout.add(passwordField, passwordConfirmField, submit);
+
+        return layout;
+    }
+
+    private void doBindFormToValidationRules(Binder<User> binder) {
+        // Shorthand for cases without extra configuration
+        binder.forField(passwordField)
+        .asRequired("This field is required")
+        .withValidator(value -> isValidPassword(value), "Password must be >=8 characters ")
+        .bind(User::getPassword,  User::setPassword);
+
+        // handle confirmPassword
+        binder.forField(passwordConfirmField)
+        .asRequired("This field is required")
+        .withValidator(value -> value.equals(passwordField.getValue()), "Passwords do not match.")
+        .bind(User::getPassword,  User::setPassword);
+    }
+
+    private boolean isValidPassword(String password) {
+        if (password.isEmpty()) return false;
+        // Perform validation logic and return true or false based on the result
+        // e.g., check if the password meets the required complexity rules
+        return password.length() >= 8;
+    }
+
+    public PasswordField getPasswordField() { return passwordField; }
+
+    public PasswordField getPasswordConfirmField() { return passwordConfirmField; }
+
+    public Span getErrorMessageField() { return errorMessageField; }
+
+    public Button getSubmitButton() { return submit; }
+}
---
+++ src/main/java/trackour/trackour/views/forgotPassword/ResetPasswordView.java
@@ -0,0 +1,99 @@
+package trackour.trackour.views.forgotPassword;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+//import com.vaadin.flow.component.button.Button;
+//import com.vaadin.flow.component.html.H3;
+//import com.vaadin.flow.component.html.Span;
+//import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+//import com.vaadin.flow.component.textfield.PasswordField;
+import com.vaadin.flow.router.BeforeEnterEvent;
+import com.vaadin.flow.router.BeforeEnterObserver;
+import com.vaadin.flow.router.BeforeEvent;
+import com.vaadin.flow.router.BeforeLeaveEvent;
+import com.vaadin.flow.router.BeforeLeaveObserver;
+import com.vaadin.flow.router.HasUrlParameter;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.auth.AnonymousAllowed;
+
+import trackour.trackour.models.CustomUserDetailsService;
+import trackour.trackour.models.User;
+import trackour.trackour.security.SecurityViewHandler;
+//import trackour.trackour.views.signup.CustomSignupForm;
+
+@Route("resetPassword/token")
+@PageTitle("Reset Password")
+@AnonymousAllowed
+
+public class ResetPasswordView extends VerticalLayout implements BeforeLeaveObserver, BeforeEnterObserver, HasUrlParameter<String> {
+
+    @Autowired
+    SecurityViewHandler securityViewHandler;
+
+    @Autowired
+    CustomUserDetailsService userService;
+
+    ResetPasswordForm resetPasswordForm;
+
+    User user;
+
+    String token;
+
+    @Override
+    public void beforeEnter(BeforeEnterEvent beforeEnterEvent) {
+        // this method call reroutes get requests to this view if the current session is already authenticated
+        this.securityViewHandler.handleAnonymousOnly(beforeEnterEvent, true);
+        if (beforeEnterEvent.getLocation()
+                .getQueryParameters()
+                .getParameters()
+                .containsKey("error")) {
+        }
+    }
+
+    @Override
+    public void beforeLeave(BeforeLeaveEvent event) {
+        // reroute to error page
+        if (event.hasUnknownReroute()){
+            System.out.println("Rerouting to Error Page!");
+        }
+    }
+
+    @Override
+    public void setParameter(BeforeEvent event, String parameter){
+        this.token = parameter;
+
+        if (userService.getByPasswordResetToken(parameter).isPresent()) {
+            this.user = userService.getByPasswordResetToken(parameter).get();
+
+            this.resetPasswordForm = new ResetPasswordForm(userService, user);
+
+            //System.out.println(token);
+        
+            // Center the form
+            setAlignItems(FlexComponent.Alignment.CENTER);
+
+            add(resetPasswordForm);
+            
+
+            //System.out.println("Found user " + user.getUsername());
+        }
+    }
+
+    public ResetPasswordView(SecurityViewHandler securityViewHandler, CustomUserDetailsService userService) {
+        this.userService = userService;
+        /*
+        this.resetPasswordForm = new ResetPasswordForm(userService, user);
+
+        System.out.println(token);
+        
+        // Center the form
+        setAlignItems(FlexComponent.Alignment.CENTER);
+
+        add(resetPasswordForm);
+        */
+    }
+    
+}
