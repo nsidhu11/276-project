6c9f8c2dedde76a1fde2be0c23521a0228a6198a
Honey-Sekhon <sekhonhoneypal@gmail.com>
2023-07-03 19:43:52 -0700

    Added Buttons To NavBAr

--- src/main/java/trackour/trackour/views/home/HomeView.java
+++ src/main/java/trackour/trackour/views/home/HomeView.java
@@ -6,11 +6,14 @@ import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.theme.lumo.LumoIcon;
 
 import jakarta.annotation.security.RolesAllowed;
 import trackour.trackour.models.CustomUserDetailsService;
@@ -18,13 +21,15 @@ import trackour.trackour.security.SecurityService;
 import trackour.trackour.security.SecurityViewHandler;
 
 @Route("")
-// Admins are users but also have the "admin" special role so pages that can be viewed by
+// Admins are users but also have the "admin" special role so pages that can be
+// viewed by
 // both users and admins should have the admin role specified as well
-@RolesAllowed({"ADMIN", "USER"})
+@RolesAllowed({ "ADMIN", "USER" })
 public class HomeView extends VerticalLayout {
-    public HomeView(SecurityViewHandler securityViewHandler, SecurityService securityService, CustomUserDetailsService customUserDetailsService) {
+    public HomeView(SecurityViewHandler securityViewHandler, SecurityService securityService,
+            CustomUserDetailsService customUserDetailsService) {
         H1 header = new H1("Trackour");
-        
+
         String sessionUsername = securityService.getAuthenticatedUser().getUsername();
         // since logged in, no need to verify if this optional is empty
         String displayNameString = customUserDetailsService.getByUsername(sessionUsername).get().getDisplayName();
@@ -48,14 +53,20 @@ public class HomeView extends VerticalLayout {
         languageComboBox.setItems("English", "Punjabi", "Spanish", "French", "German", "Hindi");
 
         TextField searchField = new TextField();
-        searchField.setPlaceholder("Search Any Music");
+        searchField.setPlaceholder("Search Music");
+        searchField.setPrefixComponent(new Icon("lumo", "search"));
 
-        HorizontalLayout topNavButtons = new HorizontalLayout(displayNameTxt, LoginButton);
+        Button mediaShelfButton = new Button("Media Shelf", new Icon(VaadinIcon.MUSIC));
+        Button exploreButton = new Button("Explore");
+
+        HorizontalLayout topNavButtons = new HorizontalLayout(mediaShelfButton, displayNameTxt, LoginButton);
         topNavButtons.setDefaultVerticalComponentAlignment(FlexComponent.Alignment.CENTER);
         topNavButtons.getStyle().set("gap", "10px"); // Add spacing between the buttons
 
         // Create a layout for the header and buttons
-        HorizontalLayout topNavBar = new HorizontalLayout(header, searchField, languageComboBox, topNavButtons);
+        searchField.setWidthFull();
+        HorizontalLayout topNavBar = new HorizontalLayout(header, exploreButton, searchField, languageComboBox,
+                topNavButtons);
         topNavBar.setAlignItems(FlexComponent.Alignment.CENTER);
         topNavBar.setWidthFull();
         topNavBar.expand(header);
